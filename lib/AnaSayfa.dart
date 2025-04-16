92e4814c28cdeb2e702741cc685095f32b8a57e8
[Kaan Can Kurt] <kaancan368368@gmail.com>
2025-04-17 02:29:13 +0300

    birkac degisiklik 17nisan

--- lib/AnaSayfa.dart
+++ lib/AnaSayfa.dart
@@ -1,8 +1,8 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:beansocial/footerr.dart';
 import 'package:beansocial/header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:beansocial/footerr.dart';
 import 'package:lottie/lottie.dart';
 
 class AnaSayfa extends StatefulWidget {
